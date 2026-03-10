d6487171b3612aced988abbb1f9ebe4fdf71b417
Eetu Palo <eetu.palo@gmail.com>
2026-03-10 17:18:32 +0200

    first commit

---
+++ Contents/mods/LaniZTeamSpawns/common/media/maps/Team_Alpha_Start/spawnpoints.lua
@@ -0,0 +1,7 @@
+function SpawnPoints()
+    return {
+        unemployed = {
+            { posX = 10830, posY = 9785, posZ = 0 },
+        }
+    }
+end
---
+++ Contents/mods/LaniZTeamSpawns/common/media/maps/Team_Bravo_Start/spawnpoints.lua
@@ -0,0 +1,7 @@
+function SpawnPoints()
+    return {
+        unemployed = {
+            { posX = 10865, posY = 10034, posZ = 0 }
+        }
+    }
+end
